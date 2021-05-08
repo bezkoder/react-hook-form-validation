7905334b5f0450fb59267026329220eff7d07ec5
Tien Nguyen Truong <tien.ngtruong@gmail.com>
2021-05-08 10:17:41 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 function App() {
+  const validationSchema = Yup.object().shape({
+    fullname: Yup.string().required('Fullname is required'),
+    username: Yup.string()
+      .required('Username is required')
+      .min(6, 'Username must be at least 6 characters')
+      .max(20, 'Username must not exceed 20 characters'),
+    email: Yup.string()
+      .required('Email is required')
+      .email('Email is invalid'),
+    password: Yup.string()
+      .required('Password is required')
+      .min(6, 'Password must be at least 6 characters')
+      .max(40, 'Password must not exceed 40 characters'),
+    confirmPassword: Yup.string()
+      .required('Confirm Password is required')
+      .oneOf([Yup.ref('password'), null], 'Confirm Password does not match'),
+    acceptTerms: Yup.bool().oneOf([true], 'Accept Terms is required')
+  });
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(validationSchema)
+  });
+
+  const onSubmit = data => {
+    console.log(JSON.stringify(data, null, 2));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="register-form">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label>Full Name</label>
+          <input
+            name="fullname"
+            type="text"
+            {...register('fullname')}
+            className={`form-control ${errors.fullname ? 'is-invalid' : ''}`}
+          />
+          <div className="invalid-feedback">{errors.fullname?.message}</div>
+        </div>
+
+        <div className="form-group">
+          <label>Username</label>
+          <input
+            name="username"
+            type="text"
+            {...register('username')}
+            className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+          />
+          <div className="invalid-feedback">{errors.username?.message}</div>
+        </div>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            name="email"
+            type="text"
+            {...register('email')}
+            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+          />
+          <div className="invalid-feedback">{errors.email?.message}</div>
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            name="password"
+            type="password"
+            {...register('password')}
+            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+          />
+          <div className="invalid-feedback">{errors.password?.message}</div>
+        </div>
+
+        <div className="form-group">
+          <label>Confirm Password</label>
+          <input
+            name="confirmPassword"
+            type="password"
+            {...register('confirmPassword')}
+            className={`form-control ${
+              errors.confirmPassword ? 'is-invalid' : ''
+            }`}
+          />
+          <div className="invalid-feedback">
+            {errors.confirmPassword?.message}
+          </div>
+        </div>
+
+        <div className="form-group form-check">
+          <input
+            name="acceptTerms"
+            type="checkbox"
+            {...register('acceptTerms')}
+            className={`form-check-input ${
+              errors.acceptTerms ? 'is-invalid' : ''
+            }`}
+          />
+          <label htmlFor="acceptTerms" className="form-check-label">
+            I have read and agree to the Terms
+          </label>
+          <div className="invalid-feedback">{errors.acceptTerms?.message}</div>
+        </div>
+
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">
+            Register
+          </button>
+          <button
+            type="button"
+            onClick={reset}
+            className="btn btn-warning float-right"
+          >
+            Reset
+          </button>
+        </div>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
